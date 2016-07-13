30a4f53398c4e7783779698f4809d40d75ec8014
Shai Davis <shaidavis@MBP.local>
2016-07-13 15:39:15 +0300

    basic functionality of reservations program are working

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
-var reservations = {
-  'Bob': { claimed: false },
-  'Ted': { claimed: true }
+var button = document.getElementById("theButton"),
+value =  button.form.valueId.value;
+button.onclick = function() {
+    foo(value);
 }
 
-var name = prompt('Please enter the name for your reservation');
+var reservations = {
+  'Bob': { claimed: false },
+  'Ted': { claimed: true },
+}
+
+// var name = prompt('Please enter the name for your reservation');
+name = name.charAt(0).toUpperCase() + name.substr(1).toLowerCase(); // capitalize the first letter of the given name and make the rest of the string lower case
 
 var claimReservation = function () {
-  // write your code here!
+	
+	// reservations[name] = reservations[name.charAt(0).toUpperCase() + name.substr(1).toLowerCase()];
+
+	if (reservations.hasOwnProperty(name)) 
+	{
+		if (reservations[name].claimed === false) {
+			alert("Come on in, " + name + "!");
+		} else if (reservations[name].claimed === true) {
+			alert("You're already here, " + name + "!");		
+		} 
+	} else {
+			// alert("No dice, " + name + "!"); 
+			reservations[name] = name;
+			reservations[name].claimed = true;
+			alert("Good news! We found you a table, " + name + "!");
+		}
 }
 
 claimReservation();
+
+
